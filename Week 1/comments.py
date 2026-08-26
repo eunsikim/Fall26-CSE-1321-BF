dd331074810fb162d197395ad78c1a8d581e3358
eunsikim <kimeunsik229@gmail.com>
2026-08-26 17:19:42 -0400

    in class 08/26

---
+++ Week 1/comments.py
@@ -0,0 +1,10 @@
+# This is a single-line comment
+
+print("Hello") # Anything to the right side of the `#` is treated as comment, left side is not a comment
+
+"""
+This is
+a multi-line
+comment
+yay!
+"""
